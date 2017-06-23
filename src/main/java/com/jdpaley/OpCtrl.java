04e6727972c175d0a7846f45ea93c96cf8175858
George Paley <George@JDPaley.com>
2017-06-23 17:15:00 -0400

    EoB

--- src/main/java/com/jdpaley/OpCtrl.java
+++ src/main/java/com/jdpaley/OpCtrl.java
@@ -1,5 +1,7 @@
 package com.jdpaley;
 
+import java.io.IOException;
+
 //import java.util.ArrayList;
 //import java.util.Collection;
 
@@ -12,6 +14,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -53,7 +59,27 @@ public class OpCtrl {
     	
     	/*m.addAttribute("WebData", wdo);
     	String choice = wdo.getAct();*/
-		System.out.println(m);
+    	
+    	try {
+//    	System.out.println("\n\nModel as JSON:\n");
+    	ObjectMapper mapper = new ObjectMapper();
+			// Convert object to JSON string
+			String jsonInString;
+//			jsonInString = mapper.writeValueAsString(m);
+//			System.out.println(jsonInString);
+	    	System.out.println("\n\nModel as pretty-printed JSON:\n");
+			// Convert object to JSON string and pretty print
+			jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(m);
+			System.out.println(jsonInString);    	    	
+    	System.out.println(" ");
+    	
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		//opRepo.save(op);
 		return "done";
