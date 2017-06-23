40ad55c7208b37dfbceefc16eb45e6b4b4a07979
George Paley <George@JDPaley.com>
2017-06-23 16:36:35 -0400

    Approaching EoB

---
+++ src/main/java/com/jdpaley/MyOwnWebDataObjectNoFreakinRepoInvolved.java
@@ -0,0 +1,21 @@
+package com.jdpaley;
+
+public class MyOwnWebDataObjectNoFreakinRepoInvolved {
+	
+	private Long acct;
+	private String act; //action to be performed
+	
+	public Long getAcct() {
+		return acct;
+	}
+	public void setAcct(Long acct) {
+		this.acct = acct;
+	}
+	public String getAct() {
+		return act;
+	}
+	public void setAct(String act) {
+		this.act = act;
+	}
+
+}
--- src/main/java/com/jdpaley/OpCtrl.java
+++ src/main/java/com/jdpaley/OpCtrl.java
@@ -1,9 +1,9 @@
 package com.jdpaley;
 
-import java.util.ArrayList;
-import java.util.Collection;
+//import java.util.ArrayList;
+//import java.util.Collection;
 
-import javax.validation.Valid;
+//import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +12,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by jdp on 6/23/17.
@@ -25,27 +25,48 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class OpCtrl {
     @Autowired
     private OpRepo opRepo;
+    
+    /**
+     * This creates a new choice object for the empty form and stuffs it into
+     * the model
+     */
+    @ModelAttribute("WebData")
+    public MyOwnWebDataObjectNoFreakinRepoInvolved supplyWebData() {
+    	MyOwnWebDataObjectNoFreakinRepoInvolved wdo = new MyOwnWebDataObjectNoFreakinRepoInvolved();
+//    	wdo.setAcct(0);
+//    	wdo.setAct("");
+      return wdo;
+    }
 
     @GetMapping(path="/")
     public String webOp(Model m)
             {
                 Op op = new Op();
                 m.addAttribute("op", op);
-                System.out.println(m);
+                //System.out.println(m);
                 return "index";
             }
  	
     @PostMapping(path="/")
-    public String doOp(@ModelAttribute("op") Op op, BindingResult bindingResult, Model m)
-    	{
-    	System.out.println("-----------------------------------");
-    	System.out.println(m);
-    	System.out.println("-----------------------------------");
-    	m.addAttribute("op", op);
+    public String getWebAct(Model m)
+    	{   	
+    	
+    	/*m.addAttribute("WebData", wdo);
+    	String choice = wdo.getAct();*/
 		System.out.println(m);
+		
+		//opRepo.save(op);
+		return "done";
+		}
+    
+    /*    @PostMapping(path="/")
+    public String doOp(@ModelAttribute("op") Op op, BindingResult bindingResult, Model m)
+    	{   	
+    	String choice = m.getValue("op");
+    	
+    	m.addAttribute("op", op);
 		opRepo.save(op);
 		return "done";
-		
-    	}
+		}*/
 	
 }
