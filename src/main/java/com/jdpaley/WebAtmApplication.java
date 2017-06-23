61b447bf297ca22fb29221649cf11cbdb79c1829
George Paley <George@JDPaley.com>
2017-06-23 09:57:49 -0400

    first commit

---
+++ src/main/java/com/jdpaley/WebAtmApplication.java
@@ -0,0 +1,12 @@
+package com.jdpaley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAtmApplication.class, args);
+	}
+}
